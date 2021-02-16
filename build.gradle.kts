166aa9f44b31723688c8217378c616742e8e819d
dfsek <dfsek@protonmail.com>
2021-02-16 11:20:06 -0700

    add bStats

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     java
+    id("com.github.johnrengelman.shadow").version("6.1.0")
 }
 
 group = "com.dfsek"
@@ -9,14 +12,20 @@ repositories {
     mavenCentral()
     maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
     maven { url = uri("https://mvn.lumine.io/repository/maven-public/") }
-    flatDir {
-        dirs("../libs")
-    }
+    maven { url = uri("https://repo.codemc.org/repository/maven-public") }
 }
 
 dependencies {
     testImplementation("junit", "junit", "4.12")
     compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-    compileOnly(fileTree("./libs/"))
+    compileOnly("com.dfsek.terra.common:common:4.2.0-BETA+b3a4c3af")
+    compileOnly("com.dfsek.terra.bukkit:bukkit:4.2.0-BETA+48296fb1")
     compileOnly("io.lumine.xikage:MythicMobs:4.9.1")
+    compileOnly("com.dfsek:Tectonic:1.3.0")
+    compileOnly("com.dfsek:Paralithic:0.3.2")
+    implementation("org.bstats:bstats-bukkit:1.7")
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    relocate("org.bstats.bukkit", "com.dfsek.terramm.lib.bstats")
 }
--- src/main/java/com/dfsek/terramm/TerraMythicMobsAddon.java
+++ src/main/java/com/dfsek/terramm/TerraMythicMobsAddon.java
@@ -3,6 +3,7 @@ package com.dfsek.terramm;
 import com.dfsek.terra.api.core.TerraPlugin;
 import com.dfsek.terra.api.core.event.EventListener;
 import com.dfsek.terra.api.core.event.events.config.ConfigPackPreLoadEvent;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +12,11 @@ public class TerraMythicMobsAddon extends JavaPlugin implements EventListener {
     public void onLoad() {
         TerraPlugin main = (TerraPlugin) Bukkit.getPluginManager().getPlugin("Terra");
         main.getEventManager().registerListener(this);
+    }
+
+    @Override
+    public void onEnable() {
+        new Metrics(this, 10356);
     }
 
     public void onPackLoad(ConfigPackPreLoadEvent event) {
